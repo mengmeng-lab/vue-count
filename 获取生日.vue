0093de930cf3e30bad3f4cbd6114429ce971a164
大萌 <65117107+mengmeng-lab@users.noreply.github.com>
2022-03-21 17:37:11 +0800

    Create 获取生日.vue

---
+++ 获取生日.vue
@@ -0,0 +1,24 @@
+// 获取生日  
+getBirth(card) {
+  var len = card.length;
+  //身份证15位时，次序为省（3位）市（3位）年（2位）月（2位）日（2位）校验位（3位），皆为数字  
+  if (len == '15') {
+      var re_fifteen = /^(\d{6})(\d{2})(\d{2})(\d{2})(\d{3})$/;
+      var arr_data = card.match(re_fifteen);
+      var year = arr_data[2];
+      var month = arr_data[3];
+      var day = arr_data[4];
+      var birthday = new Date('19' + year + '/' + month + '/' + day);
+      return birthday
+  }
+  //身份证18位时，次序为省（3位）市（3位）年（4位）月（2位）日（2位）校验位（4位），校验位末尾可能为X  
+  if (len == '18') {
+      var re_eighteen = /^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/;
+      var arr_data = card.match(re_eighteen);
+      var year = arr_data[2];
+      var month = arr_data[3];
+      var day = arr_data[4];
+      var birthday = new Date(year + '/' + month + '/' + day);
+      return birthday
+  }
+},
